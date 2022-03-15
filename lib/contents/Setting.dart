b942d9cd8d26e56be0b0b8af15d9cd5f7b177176
quynhtram01 <101619592+quynhtram01@users.noreply.github.com>
2022-03-15 15:13:20 +0700

    Update Setting.dart

--- lib/contents/Setting.dart
+++ lib/contents/Setting.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../main.dart';
 
 class Setting extends StatefulWidget {
   const Setting({Key? key}) : super(key: key);
@@ -17,6 +18,79 @@ class _SettingState extends State<Setting> {
         centerTitle: true,
         backgroundColor: Colors.indigo,
       ),
-    );
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+      TextField(
+      style: TextStyle(color: Colors.black),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        hintText: 'Tìm kiếm',
+        hintStyle: TextStyle(color: Colors.black),
+        prefixIcon: Icon(Icons.search, color: Colors.black),
+        filled: true,
+        fillColor: Colors.white12,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: Colors.black),
+        ),
+      ),
+    ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                hintText: 'Chế độ',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: IconButton(icon: Icon(MyApp.themeNotifier.value == ThemeMode.light
+    ? Icons.dark_mode
+        : Icons.light_mode ),
+    onPressed: () {
+    MyApp.themeNotifier.value =
+    MyApp.themeNotifier.value == ThemeMode.light
+    ? ThemeMode.dark
+        : ThemeMode.light;
+    }),
+                filled: true,
+                fillColor: Colors.black12,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                hintText: 'Font chữ',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.assignment_sharp, color: Colors.black),
+                filled: true,
+                fillColor: Colors.white12,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                hintText: 'Đăng xuất',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.person, color: Colors.black),
+                filled: true,
+                fillColor: Colors.white12,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+
+     ]));
+
   }
-}
+}// TODO Implement this library.
